0a2ebd6f7d101d525300aa666468562495f159da
张乃东 <jsyc_znd@163.com>
2020-11-23 16:30:41 +0800

    金智vue模板

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import style from './styles/base.less';
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="index-page">
+        <div class="page" ml5 mt12>我是首页</div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+<style lang="less" scoped>
+.index-page {
+    .page {
+        font-size: 20px;
+    }
+}
+</style>
+
+
